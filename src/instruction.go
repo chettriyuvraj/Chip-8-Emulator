746f7129614fd92f8cf351e0b2edcf059cbe230e
Yuvraj Chettri <yuvrajchettri@Yuvrajs-MacBook-Pro-2.local>
2025-03-13 21:04:28 +0530

    feat: add constant values for nibble type

--- src/instruction.go
+++ src/instruction.go
@@ -9,6 +9,25 @@ type nibble uint8 // leftmost 4 bits will always be zero
 
 type instruction uint16
 
+const (
+	NIBBLE_0  = nibble(0)
+	NIBBLE_1  = nibble(1)
+	NIBBLE_2  = nibble(2)
+	NIBBLE_3  = nibble(3)
+	NIBBLE_4  = nibble(4)
+	NIBBLE_5  = nibble(5)
+	NIBBLE_6  = nibble(6)
+	NIBBLE_7  = nibble(7)
+	NIBBLE_8  = nibble(8)
+	NIBBLE_9  = nibble(9)
+	NIBBLE_10 = nibble(10)
+	NIBBLE_11 = nibble(11)
+	NIBBLE_12 = nibble(12)
+	NIBBLE_13 = nibble(13)
+	NIBBLE_14 = nibble(14)
+	NIBBLE_15 = nibble(15)
+)
+
 // ------------------------------------------------
 // First nibble doesn't seem to have any specific name unlike other nibbles
 // ------------------------------------------------
@@ -66,6 +85,13 @@ func (i instruction) nn() byte {
 	res >>= 4
 	nn := byte(res)
 	return nn
+
+	// or
+	// y := i.y() // third nibble
+	// n := i.n() // fourth nibble
+	// y <<= 4
+	// nn := byte(y | n)
+	// return nn
 }
 
 // ------------------------------------------------
@@ -77,4 +103,21 @@ func (i instruction) nnn() uint16 {
 	res := val & mask
 	nnn := res
 	return nnn
+
+	// or
+	// x := i.x()
+	// y := i.y() // third nibble
+	// n := i.n() // fourth nibble
+	// xExtended := uint16(x)
+	// yExtended := uint16(y)
+	// nExtended := uint16(n)
+	// xExtended <<= 8
+	// yExtended <<= 4
+	// nnn := xExtended | yExtended | nExtended
+	// return nnn
+}
+
+func (n nibble) equals(val uint8) bool {
+	nVal := uint8(n)
+	return nVal == val
 }
